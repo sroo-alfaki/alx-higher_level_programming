2537252984fc2a10651c268e09bf63327c82ad85
saria <srooalfakiahmed@gmail.com>
2024-01-15 21:50:47 +0300

    list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if i < 0 or i >= len(my_list):
+        return my_list
+    my_list_copy = my_list.copy()
+    my_list_copy[i] = element
+    return my_list_copy
+
