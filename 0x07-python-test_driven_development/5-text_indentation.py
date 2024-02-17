9a7f20e41e574856479ac9a6405b32119d630e1b
saria <srooalfakiahmed@gmail.com>
2024-02-17 05:23:43 +0300

    tt

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,13 +2,7 @@
 """Module for text_indentation method."""
 
 def text_indentation(text):
-    """Method for adding 2 new lines after '.?:' chars.
-    Args:
-        text: The str text.
-
-    Raises:
-        TypeError: If text is not a str.
-    """
+    """Method for adding 2 new lines after '.?:' chars."""
 
         if not isinstance(text, str):
             raise TypeError("text must be a string")
