de700be397dc7b88ab4ac2f2221ef518dbbf3adf
saria <srooalfakiahmed@gmail.com>
2024-02-17 05:46:50 +0300

    tt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,6 +2,19 @@
 """Module for add_integer method."""
 
 def add_integer(a, b=98):
+    """Adds two integers.
+
+    Args:
+        a: the first integer.
+        b: the second integer, default 98.
+
+    Raises:
+        TypeError: if a, b are not int, float.
+
+    Returns:
+        The sum of the two integers 
+    """
+
     if type(a) not in (int, float):
         raise TypeError('a must be an integer')
     if type(b) not in (int, float):
