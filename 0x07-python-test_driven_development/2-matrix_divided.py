dc7f8b8b267f9cc53b4993aa94996f12f8488558
saria <srooalfakiahmed@gmail.com>
2024-02-17 05:33:46 +0300

    tt

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,17 +6,14 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a matrix (list of lists) " + "of integers/floats")
-                                
     for row in matrix:
         if not isinstance(row, list) or len(row) == 0:
             raise TypeError("matrix must be a matrix (list of lists) " + "of integers/floats")
-    
         if len(row) != len(matrix[0]):
-        raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix must have the same size")
         for x in row:
             if not isinstance(x, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) " + 
-                        "of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) " + "of integers/floats")
 
     return [[round(x / div, 2) for x in row] for row in matrix]
 
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,7 +4,6 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
     for delim in ".?:":
         text = (delim + "\n\n").join(
             [line.strip(" ") for line in text.split(delim)])
