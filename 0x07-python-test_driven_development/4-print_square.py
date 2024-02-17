bf2f3e8faaa95e505618805d4201f1799fd62367
saria <srooalfakiahmed@gmail.com>
2024-02-17 05:01:56 +0300

    test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for print_square method."""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print((("#" * size + "\n") * size), end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
