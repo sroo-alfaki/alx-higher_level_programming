cfacb969fa6c8d60cde05af58eaa285c083007b3
saria <srooalfakiahmed@gmail.com>
2024-02-17 05:25:59 +0300

    tt

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,16 +2,14 @@
 """Module for text_indentation method."""
 
 def text_indentation(text):
-    """Method for adding 2 new lines after '.?:' chars."""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    for delim in ".?:":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
 
-        if not isinstance(text, str):
-            raise TypeError("text must be a string")
-
-        for delim in ".?:":
-            text = (delim + "\n\n").join(
-                    [line.strip(" ") for line in text.split(delim)])
-
-            print(text, end="")
+    print(text, end="")
 
 if __name__ == "__main__":
     import doctest
